52db17b1eca07b6f1f2d7e0a065a9473e6ad6dcc
nidiqindie <a13213133779@hotmail.com>
2024-11-15 23:07:52 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   optimizeDeps: {
     include: ['canvas-confetti'],
   },
-  base: './',
+  base: '/my_game/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
